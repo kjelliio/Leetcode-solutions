a17d3161232626b7f9c83f30d8516890f9785b27
kjelliio <103134679+kjelliio@users.noreply.github.com>
2023-06-21 15:00:30 +0200

    Create Two sum.cpp

---
+++ Two sum.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for (int first = 0; first < nums.size(); first++){
+            for (int second = first + 1; second < nums.size(); second++){
+                if (nums[first] + nums[second] == target){
+				 if nums[first] + nums[second] == target then return {first, second}.
+                    return {first, second};
+                }
+            }
+        }
+        return {};
+    }
+};
